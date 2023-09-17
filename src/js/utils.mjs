131e525fab6e6d206a2840e983386dab374b8ff3
Eric w <steelheart696@gmail.com>
2023-09-16 21:52:39 -0700

    changed localstorage object to a list

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -11,7 +11,11 @@ export function getLocalStorage(key) {
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+  var allData = [];
+  const existingData = JSON.parse(localStorage.getItem(key));
+  if (Array.isArray(existingData)) allData = [... existingData];
+  allData.push(data)
+  localStorage.setItem(key, JSON.stringify(allData));
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
