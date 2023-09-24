3e2b011c8f18ecbb7680bd99d97b97b06bd5ed31
Elena Carey <theelenacarey@gmail.com>
2023-09-23 20:30:59 -0500

    Individual activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
 import {ProductData} from './ProductData.mjs';
 import {ProductList} from './ProductList.mjs'
 
-const listing = new ProductList('tents');
+const dataSource = new ProductData('tents');
+const element = document.querySelector('.product-list');
+const listing = new ProductList('Tents', dataSource, element);
+
+listing.init();
