c483436eac8817f57501448a94057af0e474abf6
Jester572 <jester572@yahoo.com>
2023-09-25 20:16:47 -0600

    Finished Remove from Cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,8 +1,8 @@
-import { getLocalStorage } from './utils.mjs';
+import { getLocalStorage, setArrLocalStorage, setLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
   const cartItems = getLocalStorage('so-cart');
-
+   
   const htmlItems = cartItems
     ? cartItems.map((item) => cartItemTemplate(item))
     : `<p><em>Your cart is empty</em></p>`;
@@ -10,10 +10,47 @@ function renderCartContents() {
   Array.isArray(htmlItems)
     ? (document.querySelector('.product-list').innerHTML = htmlItems.join(''))
     : (document.querySelector('.product-list').innerHTML = htmlItems);
+  
+  //Adds event listener to each 'X'
+  cartItems.forEach((item) => {
+    document.getElementById(item.Id).addEventListener('click', () => removeCartItem(item.Id));
+  });
 }
+
+function removeCartItem(id) {
+  //Retrieve local Storage
+  const cartItems = getLocalStorage('so-cart');
+  for (let i = 0; i < cartItems.length; i++) {
+    const item = cartItems[i];
+    if (id === item.Id) {
+      //Delete Item
+      cartItems.shift(cartItems[item])
+      //Update Local Storage
+      setLocalStorage('so-cart',cartItems);
+      renderCartContents();
+      return
+  }
+}
+
+  cartItems.forEach(item => {
+    if (id === item.Id) {
+      let red = cartItems.pop(item)
+      console.log(red);
+      return
+    }
+
+  console.log('hello');
+  setLocalStorage('so-cart',cartItems)
+  renderCartContents()
+
+  
+  });
+}
+
 
 function cartItemTemplate(item) {
   const newItem = `<li class='cart-card divider'>
+  <div id='${item.Id}' title='Remove Item' class='remove_item'>❌</div>
   <a href='#' class='cart-card__image'>
     <img
       src='${item.Image}'
