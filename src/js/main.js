330860ebb29d29d7d1f1cb1588eeda5005017758
AmericoKutomi <americo@embra.com.br>
2023-09-28 19:28:04 -0300

    W3 Team Activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
+import { loadHeaderFooter } from './utils.mjs';
 
 const dataSource = new ProductData('tents');
 const element = document.querySelector('.product-list');
@@ -7,3 +8,5 @@ const products = new ProductList('tents', dataSource, element);
 products.setTopList(['880RR', '985RF', '985PR', '344YJ']);
 products.init();
 // console.log(products);
+
+loadHeaderFooter();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -40,3 +40,25 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
 
 }
 
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML('afterbegin', template);
+  if (callback) {
+    callback(data);
+  }
+
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate('../partials/header.html');
+  const footerTemplate = await loadTemplate('../partials/footer.html');
+  const headerElement = document.querySelector('#main-header');
+  const footerElement = document.querySelector('#main-footer');
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
+}
+
+export async function loadTemplate(path) {
+  const response = await fetch(path);
+  const template = await response.text();
+  return template;
+}
